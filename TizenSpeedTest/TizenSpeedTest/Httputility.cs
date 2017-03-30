66ace7064d7cd9bc36f721b121c9b5a5f9d8f0e3
Mosab ATCHANE <mosab.atchane@gmail.com>
2017-03-30 18:04:19 +0100

    Initial commit for the NspeedTest migration:
    *Builds correctly
    *Works on Tizen simulator

---
+++ TizenSpeedTest/TizenSpeedTest/Httputility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TizenSpeedTest
+{
+    public static class HttpUtility
+    {
+        public static Dictionary<string, string> ParseQueryString(Uri uri)
+        {
+            var query = uri.Query.Substring(uri.Query.IndexOf('?') + 1); // +1 for skipping '?'
+            var pairs = query.Split('&');
+            return pairs
+                .Select(o => o.Split('='))
+                .Where(items => items.Count() == 2)
+                .ToDictionary(pair => Uri.UnescapeDataString(pair[0]),
+                    pair => Uri.UnescapeDataString(pair[1]));
+        }
+    }
+}
---
+++ TizenSpeedTest/TizenSpeedTest/SpeedTestWebClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Xml.Serialization;
+using TizenSpeedTest;
+
+namespace NSpeedTest
+{
+    internal static class SpeedTestWebClient 
+    {
+        public static int ConnectionLimit { get; set; }
+
+        public static Uri requestUri { get; set; }
+
+        public static HttpClient client { get; set; }
+
+
+        static SpeedTestWebClient()
+        {
+            ConnectionLimit = 10;
+            client = new HttpClient();
+        }
+
+        public static async System.Threading.Tasks.Task<T> GetConfigAsync<T>(string url)
+        {
+            var data = await client.GetStringAsync(url);
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            using (var reader = new StringReader(data))
+            {
+                return (T)xmlSerializer.Deserialize(reader);
+            }
+        }
+
+        public static void SetRequestConfiguration(Uri address)
+        {
+            var tAdd = AddTimeStamp(address);
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko");
+            client.DefaultRequestHeaders.Accept.ParseAdd("text/html, application/xhtml+xml, */*");
+            //            request.CachePolicy = new RequestCachePolicy(RequestCacheLevel.BypassCache); connection limit
+
+        }
+
+
+        private static Uri AddTimeStamp(Uri address)
+        {
+            var uriBuilder = new UriBuilder(address);
+            var query = HttpUtility.ParseQueryString(address);
+            query["x"] = DateTime.Now.ToFileTime().ToString(CultureInfo.InvariantCulture);
+            uriBuilder.Query = query.ToString();
+            return uriBuilder.Uri;
+        }
+    }
+
+}
